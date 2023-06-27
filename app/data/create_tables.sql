4a9356054b0dfda6805e647b6cb5a22836a232eb
YasminaMvl <pro.yasminam@gmail.com>
2023-06-28 00:55:39 +0200

    correcting bugs: BDD working

--- app/data/create_tables.sql
+++ app/data/create_tables.sql
@@ -1,14 +1,15 @@
- BEGIN;
+BEGIN;
 
- DROP TABLE IF EXISTS "users", "recipes","favorites";
-
+DROP TABLE IF EXISTS "users", "recipes", "favorites";
 
 -- Table "users"
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  is_admin BOOLEAN NOT NULL DEFAULT false
+  password  VARCHAR(255) NOT NULL,
+  is_admin BOOLEAN NOT NULL DEFAULT false,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ
 );
 
 -- Table "recipes"
@@ -16,7 +17,9 @@ CREATE TABLE recipes (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   ingredients TEXT NOT NULL,
-  instructions TEXT NOT NULL
+  instructions TEXT NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ
 );
 
 -- Table "favorites"
@@ -34,6 +37,4 @@ CREATE INDEX idx_recipes_title ON recipes (title);
 CREATE INDEX idx_favorites_user_id ON favorites (user_id);
 CREATE INDEX idx_favorites_recipe_id ON favorites (recipe_id);
 
-
-
- COMMIT;
+COMMIT;
