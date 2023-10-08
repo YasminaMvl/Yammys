22803f39f582dccbad82c83e814028a1342a873c
YasminaMvl <pro.yasminam@gmail.com>
2023-10-08 22:08:43 +0200

    affichage des images

--- app/routes/recipeRoutes.js
+++ app/routes/recipeRoutes.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './static/images/')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, Date.now() + "--" + file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage })
+
 const recipeController = require('../controllers/recipeController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
+
 // Définir les routes pour les recettes
 router.get('/', recipeController.getAllRecipes); // Récupérer toutes les recettes
-router.post('/', authMiddleware.authenticateUser, recipeController.createRecipe); // Créer une nouvelle recette
+router.post('/', authMiddleware.authenticateUser, upload.single('image'), recipeController.createRecipe); // Créer une nouvelle recette avec téléchargement d'image
 router.get('/:id', recipeController.getRecipe); // Récupérer une recette spécifique
 router.get('/create', authMiddleware.authenticateUser, recipeController.renderCreateForm); // Afficher le formulaire de création de recette
-router.post('/create', authMiddleware.authenticateUser, recipeController.createRecipe); // Créer une nouvelle recette
+router.post('/create', authMiddleware.authenticateUser, upload.single('image'), recipeController.createRecipe);
 router.delete('/:id', authMiddleware.authenticateUser, recipeController.deleteRecipe); // Supprimer une recette
 
 // Exporter le routeur
--- index.js
+++ index.js
@@ -41,7 +41,6 @@ app.use(session({
 }));
 
 
-require('./app/config/database');
 
 
 // Middleware personnalisé pour la session
